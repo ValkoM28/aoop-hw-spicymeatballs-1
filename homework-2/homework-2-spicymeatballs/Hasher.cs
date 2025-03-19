a947649983394f55b1f68d9d5dc9e3049b4c5c8f
Michal Valko <valko.miso111@gmail.com>
2025-03-20 00:50:44 +0100

    password 'hashing' :D

--- homework-2/homework-2-spicymeatballs/Hasher.cs
+++ homework-2/homework-2-spicymeatballs/Hasher.cs
@@ -1,9 +1,23 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace homework_2_spicymeatballs;
 
 public static class Hasher
 {
     public static string Hashed(string input)
     {
-        return input;  //TODO: Implement hashing
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder builder = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                builder.Append(b.ToString("x2")); // Convert byte to hex string
+            }
+            return builder.ToString();
+        }
+
     }
 }
